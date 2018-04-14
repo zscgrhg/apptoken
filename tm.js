6020c725c65f1ffc1c36f6294db431203575e99d
zscgrhg <zscgrhg@qq.com>
2018-04-14 23:37:05 +0800

    redis

---
+++ tm.js
@@ -0,0 +1,14 @@
+var obj1=[1,2,3,7,8]
+
+var obj2=[4,5,6]
+
+
+function merge(target,source){
+
+}
+
+var ret=Object.assign(obj1,obj2)
+console.log(ret)
+
+var value = typeof JSON.stringify(null)
+console.log(value)
